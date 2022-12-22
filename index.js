6ba7d1bef01670680421b543d9ab8d5f593b1474
salvador juarez <101913799+shadownrx@users.noreply.github.com>
2022-12-22 18:53:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require ('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Bot is ready!');
+});
+
+client.login('MTA0NzQ3ODU3NjQ4OTk3MTgyMg.G79elB.qhjEbYXXbmWvpAb_vB3kjpFN-XAgQOBp4U8x34');
+
+
